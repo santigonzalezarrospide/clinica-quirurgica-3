6a7a7fe751511b0443be2ee28cefdd91490be1cc
santigonzalezarrospide <santiago.g.a16@gmail.com>
2025-02-05 21:07:46 -0300

    Update Vite config for chunk size

--- clinica-quirurgica-3/vite.config.js
+++ clinica-quirurgica-3/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Configuración para rutas relativas
   build: {
     chunkSizeWarningLimit: 1000, // Aumenta el límite para evitar advertencias
     outDir: 'dist', // Asegúrate de que la salida sea en 'dist'
